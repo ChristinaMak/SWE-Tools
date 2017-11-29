b651d16d84114f62ce7599ca4e27326c1a6938cc
ChristinaMak <ChristinaMak@users.noreply.github.com>
2017-11-29 01:17:12 -0800

    Add raffle functionality

--- SWE_Bee_Points.py
+++ SWE_Bee_Points.py
@@ -1,15 +1,20 @@
-
 import os
+import random
+import numpy
+import random
+import defaultdict
+from random import choice
 
 points_files = os.listdir("Points")
 print points_files
 users_dict = {}
+name_email_dict = defaultdict()
 
 # Add points to user's point count
 def add_points(line, point_value, email_col_idx):
     data = [x.strip() for x in line.split(',')]
     if users_dict.get(data[email_col_idx]) == None:
-        users_dict[data[email_col_idx]] = point_value
+        users_dict[data[email_col_idx]] = point_value 
     else:
         users_dict[data[email_col_idx]] = users_dict.get(data[email_col_idx]) + point_value
 
@@ -19,10 +24,10 @@ def add_envision_points(line, email_col_idx):
     for cell in data:
         if cell.lower() == "x":
             if users_dict.get(data[email_col_idx]) == None:
-                users_dict[data[email_col_idx]] = 1
+                users_dict[data[email_col_idx]] = 1 
             else:
                 users_dict[data[email_col_idx]] = users_dict.get(data[email_col_idx]) + 1.0
-
+        
 # Return the index of the column with emails
 def find_email_col(fields):
     email_col_idx = -1
@@ -39,31 +44,58 @@ def isfloat(input):
     except ValueError:
         return False
 
-for p_file in points_files:
-    p_file_path = "Points/" + p_file
-    with open(p_file_path) as f:
-        if isfloat(p_file[1:4]):
-            header = f.readline()  # Get header line
-            fields = [x.strip() for x in header.split(',')]
-            email_col_idx = find_email_col(fields)
-            if email_col_idx < 0:
-                print(p_file + " does not have an email field.")
+def count_points(isRaffle):
+    for p_file in points_files: 
+        p_file_path = "Points/" + p_file
+        with open(p_file_path) as f:
+            if isfloat(p_file[1:4]):
+                points_value = 0.0
+                if isRaffle:
+                    points_value = 1.0
+                else:
+                    points_value = float(p_file[1:4])
+                
+                header = f.readline()  # Get header line
+                fields = [x.strip() for x in header.split(',')]
+                email_col_idx = find_email_col(fields)
+                if email_col_idx < 0:
+                    print(p_file + " does not have an email field.")
+                else:
+                    for line in f:
+                        add_points(line, points_value, email_col_idx)
+            elif p_file[:8].lower() == "envision": 
+                if isRaffle == False:
+                    for line in f:
+                        add_envision_points(line, 1)
             else:
-                for line in f:
-                    add_points(line, float(p_file[1:4]), email_col_idx)
-        elif p_file[:8].lower() == "envision":
-            for line in f:
-                add_envision_points(line, 1)
-        else:
-            print("The file name " + p_file + " does not specify the points value correctly. Example: [2.0] points_file_name")
+                print("The file name " + p_file + " does not specify the points value correctly. Example: [2.0] points_file_name")
 
-points_file = open('SWE_BEE_Points.csv','w')
-points_file.write("Email,Points,Is Member?\n")
-for user in users_dict:
-    isMember = float(users_dict.get(user)) >= 10.0
-    if isMember:
-        print user
-    points_file.write("{0},{1},{2}\n".format(user, users_dict.get(user), isMember))
+    points_file = open('SWE_BEE_Points.csv','w')
+    points_file.write("Email,Points,Is Member?\n")
+    for user in users_dict:
+        isMember = float(users_dict.get(user)) >= 10.0
+        if isMember:
+            print user
+        points_file.write("{0},{1},{2}\n".format(user, users_dict.get(user), isMember))
 
-points_file.close()
-print users_dict
+    points_file.close()
+    print users_dict
+    
+def getList():
+    users_list = []
+    for user in users_dict:
+        i = users_dict[user]
+        while i > 0:
+            users_list.append(user)
+            i -= 1
+    return users_list
+    
+def drawWinners():
+    users_list = getList()
+    winners = numpy.random.choice(len(users_list), 3)
+    for w in winners:
+        print users_list[w]
+    
+count_points(True)
+print "start raffle"
+drawWinners()
