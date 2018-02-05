13095287c5adaea4e31d85d42f0030f729c7fb6b
ChristinaMak <ChristinaMak@users.noreply.github.com>
2018-02-04 22:56:19 -0800

    Method for arbitrary points. Print email with points.
    Need to redo CSV file system.

--- SWE_Bee_Points.py
+++ SWE_Bee_Points.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[12]:
+
+
 import os
 import random
 import numpy
@@ -18,8 +24,11 @@ def add_points(line, point_value, email_col_idx):
         users_dict[data[email_col_idx]] = point_value 
     else:
         users_dict[data[email_col_idx]] = users_dict.get(data[email_col_idx]) + point_value
+        if data[email_col_idx] == "":
+            print("QWERGHFDKJSJFSDKHFKLSJDLVJVLKJILJGILJ")
+            print(line)
 
-# Add point for Envision Committee attendance
+# Add points for Envision Committee attendance
 def add_envision_points(line, email_col_idx):
     data = [x.strip() for x in line.split(',')]
     for cell in data:
@@ -29,6 +38,20 @@ def add_envision_points(line, email_col_idx):
             else:
                 users_dict[data[email_col_idx]] = users_dict.get(data[email_col_idx]) + 1.0
         
+# Add points from manually kept spreadsheet
+# TODO code should be improved
+def add_manual_points(f):
+    email_col_idx = 2
+    points_value = 0
+    for line in f:
+        fields = line.split(',')
+        if fields[3] != "" or fields[3] == None or len(fields) < 4:
+            try:
+                points_value = int(fields[3])
+                add_points(line, points_value, email_col_idx)    
+            except:
+                print fields[3]
+
 # Return the index of the column with emails
 def find_email_col(fields):
     email_col_idx = -1
@@ -68,19 +91,31 @@ def count_points(isRaffle):
                 if isRaffle == False:
                     for line in f:
                         add_envision_points(line, 1)
+            elif p_file[:6].lower() == "manual":
+                add_manual_points(f)
             else:
                 print("The file name " + p_file + " does not specify the points value correctly. Example: [2.0] points_file_name")
 
     points_file = open('SWE_BEE_Points.csv','w')
     points_file.write("Email,Points,Is Member?\n")
-    for user in users_dict:
-        isMember = float(users_dict.get(user)) >= 10.0
-        if isMember:
-            print user
-        points_file.write("{0},{1},{2}\n".format(user, users_dict.get(user), isMember))
+    
+    #for user in users_dict:
+    #    isMember = float(users_dict.get(user)) >= 10.0
+    #    if isMember:
+    #        print user
+    #    points_file.write("{0},{1},{2}\n".format(user, users_dict.get(user), isMember))
 
+
+    # Sort users dictionary by point values
+    for key, value in sorted(users_dict.iteritems(), key=lambda (k,v): (v,k)):
+        print '{}: {}'.format(key, value)
+        isMember = float(value) >= 10.0
+        #if isMember:
+        #    print key
+        points_file.write('{0},{1},{2}\n'.format(key, value, isMember))
+        
+    #print users_dict
     points_file.close()
-    print users_dict
     
     
 def getList():
@@ -102,5 +137,32 @@ def drawWinners():
         print users_list[w]
     
 
-#count_points(False)
-drawWinners()
+count_points(False)
+#drawWinners()
+
+
+# In[38]:
+
+
+len(users_dict)
+
+
+# In[1]:
+
+
+for line in f:
+    data = [x.strip() for x in line.split(',')]
+    if users_dict.get(data[3]) == None:
+        users_dict[data[3]] = 1 
+    else:
+        users_dict[data[3]] = users_dict.get(data[3]) + 1
+        
+        def add_points(line, point_value):
+data = [x.strip() for x in line.split(',')]
+if users_dict.get(data[3]) == None:
+    #users_dict[data[3]] = point_value 
+    users_dict.setdefault(data[3], []).append("val")
+else:
+    users_dict.setdefault(data[3], []).append("val2")
+#    users_dict[data[3]] = users_dict.get(data[3]) + point_value
+
